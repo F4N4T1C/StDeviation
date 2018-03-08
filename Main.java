50f49bfdc35d71f53f4a2ed60bac09c63536d940
F4N4T1C <36743489+F4N4T1C@users.noreply.github.com>
2018-03-08 01:00:08 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,51 +1,49 @@
 package com.company;
+
 import javax.swing.*;
+
 import java.io.*;
+
 import java.util.*;
 // ---------*---------*---------*---------*---------*
 // The use of static imports is something that should be used carefully.
 // I have only ever used this technique for the project wide constants.
 //
-import java.io.FileNotFoundException;
 
 import static com.company.ProjConstants.*;
 
 public class Main {
 
-
-
     public static void main(String[] args) {
-
         // ---------*---------*---------*---------*---------*
         // Create a new object from the StDeviation Class
         //
+
         StDeviation calcSDev =  new StDeviation();
+
         String fileName;
 
-
-
-
-
         try{
-
+            // receiving the users input for the filename and accuracy and creating a scanner
             fileName = JOptionPane.showInputDialog("Enter the name of the file");// takes user input
-
             int accuracy = Integer.parseInt(JOptionPane.showInputDialog("Enter The accuracy"));//takes in the accuracy
-
             File f = new File(fileName+".txt");//Opens file
-
             Scanner s = new Scanner(f);
-
             for(int i = 0;i<MAXDATA;i++){ // This adds the numbers the user has in the file into the Data array in the StDeviation class
+
                 if(s.hasNextInt()){
+
                     calcSDev.addNewDataItem(s.nextInt());
 
-
                 }
+
             }
-            s.close();
-            calcSDev.calcVarianceF();
-            // Print lines that display the Standard deviation the mean and variance as well as 99/95/68 data
+
+            s.close(); // closes file
+
+
+            // Print lines that display the Standard deviation the mean and variance as well as 99/95/68 data and restricts accuracy based on user input
+            System.out.println("fVariance"+ calcSDev.calcVarianceF());
             System.out.println("===================================================================");
             System.out.printf("Standard Deviation: %."+accuracy+"f\n", calcSDev.calcStandardDeviation());
             System.out.printf("Mean: %."+accuracy+"f\n", calcSDev.calcAverage());
@@ -58,24 +56,12 @@ public class Main {
             System.out.printf(" and %."+accuracy+"f\n",(calcSDev.calcAverage()+calcSDev.calcStandardDeviation()*3));
             System.out.println("===================================================================");
 
-
-
-
-
         }catch(Exception e){
+
             System.out.println("What have you done D:");//Yells at people who make it explode
+
             e.printStackTrace();
+
         }
-
-
-
-
     }
-
-
-
-
-
-
-
 }
--- StDeviation.java
+++ StDeviation.java
@@ -11,8 +11,8 @@ import java.math.*;
 /**
  * Created by Robert Hubert on 21/02/2018.
  */
-public class StDeviation {
 
+public class StDeviation {
 
     // ---------*---------*---------*---------*---------*---------*---------*---------*
     // As discussed in class we are using a class populated with Project Constants to
@@ -25,12 +25,14 @@ public class StDeviation {
     //
     //         private int[] Data = new int[ProjConstants.MAXDATA];
     //
+    // creation of arrays
     private int[] Data = new int[MAXDATA];
     private double[] aVariance = new double[MAXDATA];
+    private double[] fVariance = new double[MAXDATA];
+    private int[] nVariables = new int[MAXDATA];
     private int itemCounter = 0;
-    //Gumbo is purely to agitate Mr. Hubert
 
-
+    //Gumbo is purely to agitate Mr. Hubert :)
 
     private double StDeviation = 0;
 
@@ -40,12 +42,15 @@ public class StDeviation {
     //
     // You MUST write this method and I will use it during testing
     //
+
     public void addNewDataItem(int dataItem){
         // Takes info passed on and adds it to the array
-        Data[itemCounter] = dataItem;
-        itemCounter++;
-        //System.out.println(dataItem);(for debugging)
 
+        Data[itemCounter] = dataItem; // adds a new data item
+        //System.out.println("Data "+Data[itemCounter]); < debugging
+        itemCounter++;
+
+        //System.out.println(dataItem);(for debugging)
     }
 
     // ---------*---------*---------*---------*---------*---------*---------*---------*
@@ -53,23 +58,31 @@ public class StDeviation {
     //
     // You MUST write this method and I will use it during testing
     //
+
     public int getNumberOfDataItems(){
         // The counter I made already does this so I just return it
-
-
         return itemCounter;
     }
 
+
+
     // ---------*---------*---------*---------*---------*---------*---------*---------*
+
     // The following method returns a double precision value which is the average of all
+
     // of the data values
+
     //
+
     // You MUST write this method and I will use it during testing
+
     //
+
     public double calcAverage(){
-        int gumbo;
+        int gumbo; //Is a variable I use just for getting the sum
         int average;
         gumbo = 0;
+        // adds up all the numbers then divides them by the total amount of numbers
         for(int x=0;x<itemCounter;x++){
             gumbo += Data[x];
         }
@@ -77,54 +90,112 @@ public class StDeviation {
         average = gumbo/itemCounter;
 
         //System.out.println("AVERAGE:"+ average);
+
         return average;
     }
 
+
+
     // ---------*---------*---------*---------*---------*---------*---------*---------*
+
     // The following method returns a double precision value which is the Variance of all
+
     // of the data values
+
     //
+
     // You MUST write this method and I will use it during testing
+
     //
+
     public double calcVariance(){
+
         double variance = 0;
+
         variance = 0;
+        // calculates variance
         for(int y=0;y<itemCounter;y++){
+
             aVariance[y] = (Data[y] - calcAverage())*(Data[y] - calcAverage());
+            variance += aVariance[y];
         }
-        for(int z=0;z<itemCounter;z++){
-            variance += aVariance[z];
+
+        variance = variance/itemCounter;
+
+        //System.out.println(variance);< debugging
+        return variance;
+
+    }
+// calculates frequency variance
+    public double calcVarianceF(){
+
+        double variance = 0;
+        int frequency;
+        int currentNum;
+        boolean seenBefore = false;
+        for(int o = 0; o< itemCounter;o++){// to prevent 0's from being read as variables that have been read
+            nVariables[o] = INVALID;
         }
-            variance = variance/itemCounter;
+        for (int y = 0; y < itemCounter; y++) {
+            currentNum = Data[y];
+            frequency = 0;
+            for (int z = 0; z < itemCounter; z++) {
+                seenBefore = false;
+                if (currentNum == nVariables[z]) {
+                    seenBefore = true;
+                    break;
+                }
+            }
+            for (int imRunningOutOfLetters = 0; imRunningOutOfLetters < itemCounter; imRunningOutOfLetters++) {
+
+                if (!seenBefore && currentNum == Data[imRunningOutOfLetters]) {
+                    frequency++;
+                    System.out.println(currentNum);
+                    System.out.println(frequency);
+
+                }
+                nVariables[y] = currentNum;
+            }
+
+
+            fVariance[y] = ((Data[y] - calcAverage()) * (Data[y] - calcAverage()))*frequency;
+            variance += fVariance[y];
+            System.out.println(variance);
+        }
+        //4,5,6,3,2,8,7,9,1
+        //123456789
+
+
+        variance = variance/itemCounter;
+
         //System.out.println(variance);
 
         return variance;
     }
-    public double calcVarianceF(){
-        double variance = 0;
-        for(int y=0;y<itemCounter;y++){
-            aVariance[y] = (Data[y] - calcAverage())*(Data[y] - calcAverage());
-        }
-        for(int z=0;z<itemCounter;z++){
-            variance += aVariance[z];
-        }
-        variance = variance/itemCounter;
-        //System.out.println(variance);
-        return variance;
-    }
+
+
+
 
 
     // ---------*---------*---------*---------*---------*---------*---------*---------*
+
     // The following method returns a double precision value which is the Standard
+
     // Deviation of all of the data values
+
     //
+
     // You MUST write this method and I will use it during testing
+
     //
+
     public double calcStandardDeviation(){
 
         StDeviation = Math.sqrt(calcVariance());
 
         return StDeviation;
     }
+
+
 
 }
